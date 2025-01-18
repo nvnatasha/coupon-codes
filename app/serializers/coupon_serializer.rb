901fa0a56465513ce2b1e13b575a619c6a166a50
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-01-18 14:45:57 -0600

    coupon serializers

---
+++ app/serializers/coupon_serializer.rb
@@ -0,0 +1,39 @@
+class CouponSerializer
+
+    def self.format_coupons(coupons)
+        {
+            data: coupons.map do |coupon|
+                {
+                    id: coupon.id.to_s,
+                    type: 'coupon',
+                    attributes: {
+                        name: coupon.name,
+                        code: coupon.code,
+                        discount_type: coupon.discount_type,
+                        discount_value: coupon.discount_value,
+                        status: coupon.status,
+                        merchant_id: coupon.merchant_id
+                    }
+                }
+            end
+        }
+    end
+
+
+    def self.format_coupon(coupon)
+        {
+            data: {
+                id: coupon.id.to_s,
+                type: 'coupon',
+                attributes: {
+                    name: coupon.name,
+                    code: coupon.code,
+                    discount_type: coupon.discount_type,
+                    discount_value: coupon.discount_value,
+                    status: coupon.status,
+                    merchant_id: coupon.merchant_id
+                }
+            }
+        }
+    end
+end
